7227e6bb9eadd8d9091296050084052242067cab
Roberto <robertoccamposn@gmail.com>
2020-09-08 15:20:46 -0400

    Updates

---
+++ precache-manifest.3e6322f37d1653675ec921fc9fee4955.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fe6dab72b3a369d45a2ce774f52b9f",
+    "url": "/cdbleste/index.html"
+  },
+  {
+    "revision": "70b4ef196d4fa0384733",
+    "url": "/cdbleste/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "92979e88c8423a64aed3",
+    "url": "/cdbleste/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "70b4ef196d4fa0384733",
+    "url": "/cdbleste/static/js/2.d97ce5f7.chunk.js"
+  },
+  {
+    "revision": "3ac348dd37317930601de15123d8f16e",
+    "url": "/cdbleste/static/js/2.d97ce5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92979e88c8423a64aed3",
+    "url": "/cdbleste/static/js/main.22d8b78e.chunk.js"
+  },
+  {
+    "revision": "994bc5c6c31a6711c7c6",
+    "url": "/cdbleste/static/js/runtime-main.2f9daed3.js"
+  },
+  {
+    "revision": "d19f37aeb56416736c08eb84079426c5",
+    "url": "/cdbleste/static/media/banner_matriculas_Prancheta-1.d19f37ae.png"
+  },
+  {
+    "revision": "e55acbbcabe89eb45145aae1d9387562",
+    "url": "/cdbleste/static/media/edebe.e55acbbc.png"
+  },
+  {
+    "revision": "26f46069f1bddc1d8f172c12fcca2829",
+    "url": "/cdbleste/static/media/edital-bolsa-2011.26f46069.jpeg"
+  },
+  {
+    "revision": "5746215c5894728c99436cf2f0214e78",
+    "url": "/cdbleste/static/media/hotnews1.5746215c.jpg"
+  },
+  {
+    "revision": "bad59f12ee7c99b59c8acbcd9db172d9",
+    "url": "/cdbleste/static/media/isma.bad59f12.jpg"
+  },
+  {
+    "revision": "54c437c263140ce6fd4a77baa3eec9dc",
+    "url": "/cdbleste/static/media/logo-cdbl.54c437c2.png"
+  },
+  {
+    "revision": "cca1cffb4235e13036bfbcb39629ff37",
+    "url": "/cdbleste/static/media/logo-landing.cca1cffb.png"
+  },
+  {
+    "revision": "ec81f5667ba94e63bddca5381579e0c9",
+    "url": "/cdbleste/static/media/missa-fake-post.ec81f566.jpg"
+  },
+  {
+    "revision": "565b18afae74f5e80ba5d53c3a440c69",
+    "url": "/cdbleste/static/media/rede-salesiana.565b18af.png"
+  },
+  {
+    "revision": "abd0cbb5cc79221143eedd3fa1fc7773",
+    "url": "/cdbleste/static/media/section-bg.abd0cbb5.png"
+  }
+]);
