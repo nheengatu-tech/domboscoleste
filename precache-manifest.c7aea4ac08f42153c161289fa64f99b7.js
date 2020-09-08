bffde14588642c3ad2e651ce63685f487d5a9f4c
Roberto <robertoccamposn@gmail.com>
2020-09-08 15:09:42 -0400

    Updates

---
+++ precache-manifest.c7aea4ac08f42153c161289fa64f99b7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386687913c5f29547b6ed3cdd11023c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe62b4822cd4dcac039",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "92979e88c8423a64aed3",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "6fe62b4822cd4dcac039",
+    "url": "/static/js/2.bd445c83.chunk.js"
+  },
+  {
+    "revision": "3ac348dd37317930601de15123d8f16e",
+    "url": "/static/js/2.bd445c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92979e88c8423a64aed3",
+    "url": "/static/js/main.22d8b78e.chunk.js"
+  },
+  {
+    "revision": "95ac7ef86e31ace8d90a",
+    "url": "/static/js/runtime-main.5246ebbc.js"
+  },
+  {
+    "revision": "d19f37aeb56416736c08eb84079426c5",
+    "url": "/static/media/banner_matriculas_Prancheta-1.d19f37ae.png"
+  },
+  {
+    "revision": "e55acbbcabe89eb45145aae1d9387562",
+    "url": "/static/media/edebe.e55acbbc.png"
+  },
+  {
+    "revision": "26f46069f1bddc1d8f172c12fcca2829",
+    "url": "/static/media/edital-bolsa-2011.26f46069.jpeg"
+  },
+  {
+    "revision": "5746215c5894728c99436cf2f0214e78",
+    "url": "/static/media/hotnews1.5746215c.jpg"
+  },
+  {
+    "revision": "bad59f12ee7c99b59c8acbcd9db172d9",
+    "url": "/static/media/isma.bad59f12.jpg"
+  },
+  {
+    "revision": "54c437c263140ce6fd4a77baa3eec9dc",
+    "url": "/static/media/logo-cdbl.54c437c2.png"
+  },
+  {
+    "revision": "cca1cffb4235e13036bfbcb39629ff37",
+    "url": "/static/media/logo-landing.cca1cffb.png"
+  },
+  {
+    "revision": "ec81f5667ba94e63bddca5381579e0c9",
+    "url": "/static/media/missa-fake-post.ec81f566.jpg"
+  },
+  {
+    "revision": "565b18afae74f5e80ba5d53c3a440c69",
+    "url": "/static/media/rede-salesiana.565b18af.png"
+  },
+  {
+    "revision": "abd0cbb5cc79221143eedd3fa1fc7773",
+    "url": "/static/media/section-bg.abd0cbb5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdbleste/precache-manifest.3e6322f37d1653675ec921fc9fee4955.js"
+  "/precache-manifest.c7aea4ac08f42153c161289fa64f99b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdbleste/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
