d00915bab60d6e4d874eefbcee024798d211fe5f
Roberto <robertoccamposn@gmail.com>
2020-10-01 16:52:38 -0400

    feat: add redirects to social medias

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,56 +1,71 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import {
   ContactsComponent,
   ContactPanel,
   Container,
   Contact,
   SocialMedia,
-} from './styles'
-import { FiYoutube } from 'react-icons/fi'
-import { FiFacebook } from 'react-icons/fi'
-import { FiInstagram } from 'react-icons/fi'
-import { FaWhatsapp } from 'react-icons/fa'
+} from "./styles";
+import { FiYoutube } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Header = () => {
-  const [contacts, setContact] = useState(false)
+  const [contacts, setContact] = useState(false);
 
   const showContacts = () => {
-    setContact(!contacts)
-  }
+    setContact(!contacts);
+  };
 
-  return(
+  return (
     <Container>
-      <SocialMedia>
+      <SocialMedia
+        onClick={() => window.open("https://wa.me/+5592992599150", "_blank")}
+      >
         <FaWhatsapp />
       </SocialMedia>
-      <SocialMedia>
+      <SocialMedia
+        onClick={() =>
+          window.open(
+            "https://www.facebook.com/colegiodomboscolestemanaus",
+            "_blank"
+          )
+        }
+      >
         <FiFacebook />
       </SocialMedia>
-      <SocialMedia>
+      <SocialMedia
+        onClick={() =>
+          window.open(
+            "https://www.instagram.com/colegiodomboscoleste/",
+            "_blank"
+          )
+        }
+      >
         <FiInstagram />
       </SocialMedia>
-      <SocialMedia>
+      <SocialMedia
+        onClick={() =>
+          window.open(
+            "https://www.youtube.com/channel/UCb5m5OvVCytJJ_SuQayWhEA",
+            "_blank"
+          )
+        }
+      >
         <FiYoutube />
       </SocialMedia>
       <Contact onClick={showContacts}>
         + contatos
-        {
-          contacts
-          ?
+        {contacts ? (
           <ContactPanel>
-            <ContactsComponent>
-              (92) 99259-9150
-            </ContactsComponent>
-            <ContactsComponent>
-              (92) 3131-4100
-            </ContactsComponent>
+            <ContactsComponent>(92) 99259-9150</ContactsComponent>
+            <ContactsComponent>(92) 3131-4100</ContactsComponent>
           </ContactPanel>
-          :
-          null
-        }
+        ) : null}
       </Contact>
     </Container>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
