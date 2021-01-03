69776a53f9b689bf3a14f3fdbd2106ab97cd64f8
Roberto Campos <robertoccamposn@gmail.com>
2021-01-03 19:17:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4566c635315adc5cbbbd0bf2cb37d6fd.js"
+  "./precache-manifest.53c714ebe41788495b00a4c47faa2cc7.js"
 );
 
 self.addEventListener('message', (event) => {
